25dfacf2b729ac6f47f7022c918559dc2888378a
J-ustine <justine.collache@gmail.com>
2022-02-07 14:48:58 +0100

    create components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FormPersonalData from "./components/FormPersonalData";
+import FormProfessionalData from "./components/FormProfessionalData";
+import Profil from "./components/Profil";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
       </header>
+      <FormPersonalData />
+      <FormProfessionalData />
+      <Profil />
     </div>
   );
 }
